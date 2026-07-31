0f12f3f37789561ec3a3500cd898c475dff72ca5
Teja <2500031053@kluniversity.in>
2026-07-31 15:25:57 +0530

    Added the dataset.html

--- app/app.py
+++ app/app.py
@@ -1,8 +1,24 @@
+from axios.navigator import first
 from flask import Flask, render_template
+
+from data.load_data import load_data, get_summary
 
 app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("home.html")
+
+
+@app.route("/dataset")
+def dataset():
+    df=load_data()
+    summary=get_summary(df)
+    return render_template(
+        "dataset.html",
+        summary=summary,
+        first_rows=df.head().to_html(index=False)
+    )
+
+
 if __name__ == "__main__":
     app.run(debug=True)
