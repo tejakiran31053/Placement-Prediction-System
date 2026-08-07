4a89fc60e9df993a4e68f40cccf8361f226833ae
Teja <2500031053@kluniversity.in>
2026-08-07 14:34:31 +0530

    modified the User Interface

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -2,12 +2,14 @@ import pandas as pd
 def load_data():
     df=pd.read_csv("C:/Users/lenovo/PycharmProjects/PlacementPredictionSystem/data/placement_data.csv")
     return df
+
+
 def get_summary(df):
-    return {
-        "rows": df.shape[0],
-        "columns": df.shape[1],
-        "target": "PlacementStatus"
+    return{
+        "rows":df.shape[0],
+        "columns":df.shape[1],
+        "target":"palcement status"
     }
 if __name__ == "__main__":
-    df=load_data()
+    df= load_data()
     print(get_summary(df))
